f00d31779e6fa63c5bed640caadcdd0a6d463433
zhangzh <zhangzh@cnlemon.net>
2019-09-04 21:36:49 +0800

    config: 配置 jest 支持 test.ts 文件

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,28 +1,4 @@
-import { add, minus } from "./math";
-class Expect {
-  constructor(private result: any) {}
-
-  toBe = (expectResult: any): void => {
-    if (this.result !== expectResult) {
-      throw new Error(
-        `预期与实际值不符: 预期: ${expectResult}; 结果:${this.result}`
-      );
-    }
-  };
-}
-
-const test = (testName: string, testFn: () => void): void => {
-  try {
-    testFn();
-    console.log(`${testName}通过测试`);
-  } catch (e) {
-    console.log(`${testName}没有通过测试: ${e}`);
-  }
-};
-
-const expect = (result: any): Expect => {
-  return new Expect(result);
-};
+import { add, minus, multi } from "./math";
 
 test("测试加法 3 + 7", () => {
   expect(add(3, 7)).toBe(10);
@@ -30,4 +6,8 @@ test("测试加法 3 + 7", () => {
 
 test("测试减法 3 - 3", () => {
   expect(minus(3, 3)).toBe(0);
+});
+
+test("测试乘法 3 * 3", () => {
+  expect(multi(3, 3)).toBe(9);
 });
